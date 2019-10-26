8222377411f54c0e01e49b2b54c8960b7b7aaaf4
alloyking1 <alloyking1@gmail.com>
2019-10-26 01:36:49 -0400

    html,css,javascript video player

---
+++ asset/js/custom.js
@@ -0,0 +1,10 @@
+
+
+alert();
+
+// function moveFile(){
+//     var object = new ActiveXObject("Scripting.FileSystemObject");
+//     var file = object.GetFile("C:\\wamp\\www\\phptest.php");
+//     file.Move("C:\\wamp\\");
+//     document.write("File is moved successfully");
+//  }
